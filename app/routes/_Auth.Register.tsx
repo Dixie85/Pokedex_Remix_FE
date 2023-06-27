831a36927cccd323d96642d9a98b53c832a5551f
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-06-28 01:12:12 +0200

    Screen adjustment

--- app/routes/_Auth.Register.tsx
+++ app/routes/_Auth.Register.tsx
@@ -38,20 +38,22 @@ export default function LogIn() {
   console.log(data);
 
   return (
-    <main className="relative flex flex-col w-screen h-screen bg-cover bg-Hero">
-      <BackButton />
-      <section className="m-auto p-2">
-        <h1 className="mb-8 font-Great text-6xl text-center">Register</h1>
-        <Form method="post" className="flex flex-col">
-          <label className="my-4 text-2xl"> E-mail: </label>
-          <input name="email" type="text" placeholder="e-mail" className="p-2 text-2xl text-orange-300 text-center bg-black/60 rounded-md placeholder:text-gray-100/90 placeholder:text-center placeholder:italic outline-none" />
-          <label className="my-4 text-2xl"> Password: </label>
-          <input name="password" type="password" placeholder="password" className="p-2 text-2xl text-orange-300 text-center bg-black/60 rounded-md placeholder:text-gray-100/90 placeholder:text-center placeholder:italic outline-none" />
-          <button type="submit" className="mt-10 px-5 py-2 bg-amber-400/80 text-2xl rounded-2xl hover:text-orange-400 hover:bg-black/20  hover:shadow-lg">Register</button>
-        </Form>
-        {data ? <p className="mt-2 text-2xl text-right">{data as string}</p> : null}
+    <main className="relative flex max-w-screen-md h-screen mx-auto  bg-cover bg-Hero">
+      <section className="flex flex-col w-screen h-screen ">
+        <BackButton />
+        <section className="m-auto p-2">
+          <h1 className="mb-8 font-Great text-6xl text-center">Register</h1>
+          <Form method="post" className="flex flex-col">
+            <label className="my-4 text-2xl"> E-mail: </label>
+            <input name="email" type="text" placeholder="e-mail" className="p-2 text-2xl text-orange-300 text-center bg-black/60 rounded-md placeholder:text-gray-100/90 placeholder:text-center placeholder:italic outline-none" />
+            <label className="my-4 text-2xl"> Password: </label>
+            <input name="password" type="password" placeholder="password" className="p-2 text-2xl text-orange-300 text-center bg-black/60 rounded-md placeholder:text-gray-100/90 placeholder:text-center placeholder:italic outline-none" />
+            <button type="submit" className="mt-10 px-5 py-2 bg-amber-400/80 text-2xl rounded-2xl hover:text-orange-400 hover:bg-black/20  hover:shadow-lg">Register</button>
+          </Form>
+          {data ? <p className="mt-2 text-2xl text-right">{data as string}</p> : null}
+        </section>
+        <InfoButton />
       </section>
-      <InfoButton />
     </main>
   );
 }
